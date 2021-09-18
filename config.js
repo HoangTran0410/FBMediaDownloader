440b61b048b4f7a1d5e0065c4a00b79e91c74d08
Hoang <th01634044850@gmail.com>
2021-09-19 01:16:45 +0700

    [Fix]

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // you can modify all the variables below
-export const ACCESS_TOKEN = "EAAAAZAw4FxQIBAON6KcVr9O2gvkVJHq1QmCc8uV2wyJTf7Uwk0vqDLmaZA39ZAD4CdXRT0Yspp7WS6zNHY9B3t5iQ8iVGbwkn7769qBJod8gn9YeERZBnQ0r8Yz5ljk7DAQhQn1HO77c36UqjtPJfNiDgQUJybiuNrkVDENkRAZDZD";
+export const ACCESS_TOKEN = "YOUR_ACCESS_TOKEN";
 export const WAIT_BEFORE_NEXT_FETCH = 0;
 export const ID_LINK_SEPERATOR = ";";
 export const PHOTO_FILE_FORMAT = 'png'; // OR jpg
