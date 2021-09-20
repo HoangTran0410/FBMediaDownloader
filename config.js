4eba1fba6987215e2b4e60b7d472afe6831c1bca
Hoang <th01634044850@gmail.com>
2021-09-20 10:02:40 +0700

    [Format] prettier

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 export const ACCESS_TOKEN = "YOUR_ACCESS_TOKEN";
 export const WAIT_BEFORE_NEXT_FETCH = 0;
 export const ID_LINK_SEPERATOR = ";";
-export const PHOTO_FILE_FORMAT = 'png'; // OR jpg
-export const VIDEO_FILE_FORMAT = 'mp4'; // OR wav ?
+export const PHOTO_FILE_FORMAT = "png"; // OR jpg
+export const VIDEO_FILE_FORMAT = "mp4"; // OR wav ?
 export const FOLDER_TO_SAVE_LINKS = "downloads/links";
-export const FOLDER_TO_SAVE_ALBUM_MEDIA = 'downloads/album_media';
-export const FOLDER_TO_SAVE_FEED_MEDIA = 'downloads/feed_media';
+export const FOLDER_TO_SAVE_ALBUM_MEDIA = "downloads/album_media";
+export const FOLDER_TO_SAVE_FEED_MEDIA = "downloads/feed_media";
