2bf8111894ae39b81b032d5c654ca0d2e732d383
Hoang <th01634044850@gmail.com>
2021-09-21 11:26:57 +0700

    [Hot Fix] from_photo_id

--- scripts/menu.js
+++ scripts/menu.js
@@ -18,7 +18,7 @@ const rl = readline.createInterface({
 const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
 
 const wait_for_key_pressed = async () =>
-  await prompt("..press any key to continue..");
+  await prompt("..Nhap phim bat ky de tiep tuc..");
 
 const choose = async (title, menu_items) => {
   let title_ui = `======== ${title} ========`;
@@ -32,14 +32,14 @@ const choose = async (title, menu_items) => {
   console.log(ui);
 
   while (true) {
-    const input = await prompt("\n> Choose action: ");
+    const input = await prompt("\n> Chon chuc nang: ");
     if (input in menu_items) {
       return {
         key: input,
         value: menu_items[input],
       };
     } else {
-      console.log("[!] Invalid. Please choose again.");
+      console.log("[!] Khong hop le. Vui long chon lai.");
     }
   }
 };
@@ -47,19 +47,22 @@ const choose = async (title, menu_items) => {
 // ========================================== MENU =========================================
 const menuDownloadAlbum = async () => {
   while (true) {
-    const action = await choose("FB Media Downloader Tool > Download Album", {
-      0: "<- Back",
-      1: "Download all PHOTOs in album",
-      2: "Download all LINKs of photos in album",
+    const action = await choose("FB Media Downloader Tool > Tai Album", {
+      0: "<- Quay lai",
+      1: "Tai tat ca ANH trong album",
+      2: "Tai tat ca LINK cua anh trong album",
     });
 
     if (action.key == 0) break;
     if (action.key == 1 || action.key == 2) {
-      const album_id = await prompt("> Enter album id (-1 to go back): ");
-      const from_photo_id = await prompt(
-        "> From photo id (0 to fetch from beginning  of album): "
+      const album_id = await prompt("> Nhap album id (Nhap -1 de quay lai): ");
+      const from_photo_id_text = await prompt(
+        "> Tai tu vi tri id anh nao? (Nhap 0 de tai tu dau album): "
       );
       if (album_id != -1) {
+        const from_photo_id =
+          from_photo_id_text == "0" ? null : from_photo_id_text;
+
         action.key == 1
           ? await downloadAlbumPhoto({
               albumId: album_id,
@@ -77,22 +80,22 @@ const menuDownloadAlbum = async () => {
 const menuDownloadWallMedia = async () => {
   while (true) {
     const action = await choose(
-      "FB Media Downloader Tool > Download Wall Media",
+      "FB Media Downloader Tool > Tai anh/video tren tuong (wall)",
       {
-        0: "<- Back",
-        1: "Download all VIDEO/PHOTOs on target's wall",
-        2: "Download all LINKs of video/photos on target's wall",
+        0: "<- Quay lai",
+        1: "Tai tat ca ANH/VIDEO tren wall cua (user/group/page)",
+        2: "Tai tat ca LINKS anh/video tren wall cua (user/group/page)",
       }
     );
 
     if (action.key == 0) break;
     if (action.key == 1 || action.key == 2) {
       const target_id = await prompt(
-        "> Enter target id (user_id/group_id/page_id) (-1 to go back): "
+        "> Nhap id cua doi tuong (user_id/group_id/page_id) (Nhap -1 de quay lai): "
       );
       if (target_id != -1) {
         const page_limit = await prompt(
-          "> Page limit (0 to download all media in target wall): "
+          "> Gioi han bao nhieu trang (Nhap 0 de tai het moi thu tren tuong): "
         );
         if (page_limit >= 0) {
           console.log("in");
@@ -115,11 +118,11 @@ const menuDownloadWallMedia = async () => {
 export const menu = async () => {
   while (true) {
     const action = await choose("FB Media Downloader Tool", {
-      1: "Get album infomation",
-      2: "Get timeline_album_id of page",
-      3: "Download album",
-      4: "Download wall media",
-      5: "Exit",
+      1: "Xem thong tin album",
+      2: "Tim timeline album cua page",
+      3: "Tai album (cua user/page/group)",
+      4: "Tai anh/video tren tuong (wall)",
+      5: "Thoat",
     });
     if (action.key == 1) {
       const album_id = await prompt("> Enter album id (-1 to go back): ");
